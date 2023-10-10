c10dda4bbb929e3f3906f89305d02e9b54091171
UmxoMetal <umxometal@gmail.com>
2023-10-10 21:32:21 +0300

    hw08-spring-data-mongo

--- hw08-spring-data-mongo/src/test/java/otus/repository/BookRepositoryTest.java
+++ hw08-spring-data-mongo/src/test/java/otus/repository/BookRepositoryTest.java
@@ -1,11 +1,9 @@
 package otus.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import otus.domain.Author;
 import otus.domain.Book;
